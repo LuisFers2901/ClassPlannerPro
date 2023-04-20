44325b62718694ca3e839d8ca824e35f088763a5
LuisFers2901 <luisf.perez1@udea.edu.co>
2023-04-20 03:23:25 -0500

    Inclusion de funciones nuevas

--- HeadCPP.h
+++ HeadCPP.h
@@ -9,4 +9,9 @@ using namespace std;
 bool PensumReading(char* &Courses, int* &IDECourses, int* &HRSCourses, int* &IndexCourses, int* &ToolsCourses);
 void ViewCourses(char* &Courses, int* &IDECourses, int* &HRSCourses, int* &IndexCourses, int* &ToolsCourses);
 void NameCourse(char* &Courses, int* &IndexCourses, int Index);
+int* CoursesOptions(char* &Courses, int* &IndexCourses, int* &ToolsCourses, int* &HRSCourses);
+void SaveMatrix(int* ScheduleMatrix,  char* outputFilename);
+void PrintMatrix(int* ScheduleMatrix);
+
+//void PrintMatrix(int* ScheduleMatrix);
 #endif // HEADCPP_H
