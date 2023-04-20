925116558194fb9a06e667e4db4ccb7cfd27f930
LuisFers2901 <luisf.perez1@udea.edu.co>
2023-04-20 03:22:19 -0500

    Eleccion de cursos y visualizacion de matriz

--- FunctionsClassPlanner.cpp
+++ FunctionsClassPlanner.cpp
@@ -126,9 +126,16 @@ void ViewCourses(char* &Courses, int* &IDECourses, int* &HRSCourses, int* &Index
 
     cout << "\n---------------- Lista de Cursos ----------------" << endl;
     for (int i = 0 ; i < ToolsCourses[1]; i++){
-        cout << i << ". (" << HRSCourses[((i*4)+3)] << "C) ";
-        NameCourse(Courses, IndexCourses, i);
-        cout << endl;
+        if (i < 10){
+            cout << i << ".  (" << HRSCourses[((i*4)+3)] << "C) ";
+            NameCourse(Courses, IndexCourses, i);
+            cout << " (" << IDECourses[i] << ")" << endl;
+        }
+        else{
+            cout << i << ". (" << HRSCourses[((i*4)+3)] << "C) ";
+            NameCourse(Courses, IndexCourses, i);
+            cout << " (" << IDECourses[i] << ")" << endl;
+        }
     }
     cout << "-------------------------------------------------" << endl;
 }
@@ -139,5 +146,222 @@ void NameCourse(char* &Courses, int* &IndexCourses, int Index){
 
         cout << Courses[i];
     }
-
 }
+
+int* CoursesOptions(char* &Courses, int* &IndexCourses, int* &ToolsCourses, int* &HRSCourses){
+
+    int* ChosenCourses = new int[25] {-1};
+    int Cont = 0;
+    int IndexM;
+    int YN;
+    int Credits = 0;
+    int CreditValue;
+    cout << endl;
+    while (true){
+
+        cout << "Ingrese la materia a matricular por su indice: ";
+        cin >> IndexM;
+        cout << "\033[F\033[K"; // Mover el cursor a la línea anterior y borrarla
+
+        while(IndexM < 0 || IndexM > (ToolsCourses[1] - 1)){
+            cout << "Opcion Invalida, use los indice de la lista: ";
+            cin >> IndexM;
+            cin.ignore();
+            cout << "\033[F\033[K"; // Mover el cursor a la línea anterior y borrarla
+        }
+
+        bool Run = true;
+
+        for (int i = 0; ChosenCourses[i] != -1; i++){
+            if (IndexM == ChosenCourses[i]){
+                cout << "No puedes repetir cursos ya selecionados\n";
+                cout << "Oprima Enter para salir";
+                cin.ignore();
+                cin.get();
+                cout << "\033[F\033[K";
+                cout << "\033[F\033[K";
+                Run = false;
+            }
+        }
+        if (Run == true){
+
+            cout << "Seguro que desea agregar la siguiente materia: ";
+            NameCourse(Courses, IndexCourses, IndexM);
+            cout << "\nIngrese 1 para Si, de lo contrario 0: ";
+            cin >> YN;
+            cin.ignore();
+            cout << "\033[F\033[K";
+            cout << "\033[F\033[K";
+
+            while(YN != 1 && YN != 0){
+                cout << "Opcion Invalida, 1 para Si, de lo contrario 0: ";
+                cin >> YN;
+                cin.ignore();
+                cout << "\033[F\033[K";
+            }
+
+            if (YN == 1){
+                CreditValue = HRSCourses[((IndexM*4)+3)];
+                if ((Credits+CreditValue) < 24){
+                    Credits += CreditValue;
+                    ChosenCourses[Cont] = IndexM;
+                    Cont++;
+                    ChosenCourses[Cont] = -1;
+                }
+                else{
+                    cout << "Limite de creditos superados\n";
+                }
+            }
+            int EndFunction = 0;
+            cout << "Actualmente cuentas con " << Credits << ". (Maximo de creditos 24)\n"
+                    "Deseas agregar otra materia? 1 para Si, de lo contrario 0: ";
+            cin >> EndFunction;
+            cin.ignore();
+            cout << "\033[F\033[K";
+            cout << "\033[F\033[K";
+
+            while(EndFunction != 1 && EndFunction != 0){
+                cout << "Opcion Invalida, 1 para Si, de lo contrario 0: ";
+                cin >> EndFunction;
+                cin.ignore();
+                cout << "\033[F\033[K";
+            }
+
+
+            cout << "Desea visualizar los cursos actualmente seleccionados?\n"
+                    "Ingrese 1 para Si, de lo contrario 0: ";
+            cin >> YN;
+            cin.ignore();
+            while(YN != 1 && YN != 0){
+                cout << "Opcion Invalida, 1 para Si, de lo contrario 0: ";
+                cin >> YN;
+                cin.ignore();
+                cout << "\033[F\033[K";
+            }
+            cout << "\033[F\033[K";
+            cout << "\033[F\033[K";
+            if (YN == 1){
+                int j = 0;
+                for (j; ChosenCourses[j] != -1; j++){
+                    cout << j + 1 << ". (" << HRSCourses[((ChosenCourses[j]*4)+3)] << ") ";
+                    NameCourse(Courses, IndexCourses, ChosenCourses[j]);
+                    cout << endl;
+                }
+
+                cout << "Total de creditos Actuales: " << Credits << endl;
+                cout << "Oprima Enter para salir ";
+                cin.ignore();
+
+                for (int i = 0; i < j+2; i++){
+                    cout << "\033[F\033[K";
+                }
+            }
+
+            if(EndFunction == 0){
+                break;
+            }
+        }
+    }
+    return ChosenCourses;
+}
+
+void SaveMatrix(int* ScheduleMatrix, char* outputFilename){
+    // ...
+    std::ofstream outputFile("sintítulo.txt"); // abrir el archivo de salida
+    int Rows = 16;
+    int Columns = 7;
+    char CenterDivision[] = "╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣";
+    char LowerDivision[]  = "╚═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╝";
+
+    // escribir la salida en el archivo en lugar de la consola
+    outputFile << "╔═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╗\n"
+                  "║   HORAS   ║   LUNES   ║  MARTES   ║ MIERCOLES ║  JUEVES   ║  VIERNES  ║  SABADO   ║  DOMINGO  ║\n"
+                  "╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣";
+    for (int i = 0; i < Rows; i++){
+
+        outputFile << "\n║";
+        if (i < 4){
+            outputFile << "   0" << (6 + i) << ":00   ║";
+        }
+        else{
+            outputFile << "   " << (6 + i) << ":00   ║";
+        }
+
+        for(int j = 0; j < Columns; j++){
+            int Index = (i* Columns) + j;
+            outputFile << "  ";
+            if(ScheduleMatrix[Index] == 0){
+                outputFile << "         ║";
+            }
+            else{
+                outputFile << ScheduleMatrix[Index]<< "  ║";
+            }
+        }
+        if(i == Rows-1){
+            break;
+        }
+
+        outputFile << endl << CenterDivision;
+    }
+    outputFile << endl << LowerDivision;
+
+    outputFile.close(); // cerrar el archivo
+}
+
+void PrintMatrix(int* ScheduleMatrix){
+
+    int Rows = 16;
+    int Columns = 7;
+    char Division[] = "+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+";
+
+    cout << "+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+\n"
+                  "|   HORAS   |   LUNES   |  MARTES   | MIERCOLES |  JUEVES   |  VIERNES  |  SABADO   |  DOMINGO  |\n"
+                  "+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+";
+    for (int i = 0; i < Rows; i++){
+
+        cout << "\n|";
+        if (i < 4){
+            cout << "   0" << (6 + i) << ":00   |";
+        }
+        else{
+            cout << "   " << (6 + i) << ":00   |";
+        }
+
+        for(int j = 0; j < Columns; j++){
+            int Index = (i* Columns) + j;
+            cout << "  ";
+            if(ScheduleMatrix[Index] == 0){
+                cout << "         |";
+            }
+            else{
+                cout << ScheduleMatrix[Index]<< "  |";
+            }
+        }
+        if(i == Rows-1){
+            break;
+        }
+
+        cout << endl << Division;
+    }
+    cout << endl << Division;
+}
+
+
+/*void PrintMatrix(int* ScheduleMatrix){
+    int Rows = 16;
+    int Columns = 7;
+    char CenterDivision[] = "╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣";
+    char LowerDivision[]  = "╚═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╩═══════════╝";
+    cout<< "╔═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╦═══════════╗"
+           "║   HORAS   ║   LUNES   ║  MARTES   ║ MIERCOLES ║  JUEVES   ║  VIERNES  ║  SABADO   ║  DOMINGO  ║"
+           "╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣";
+    for (int i = 0; i < Rows; i++){
+        cout << "\n║";
+        for(int j = 0; j < Columns; j++){
+            int Index = (i + j) * Rows;
+            cout << "  " << ScheduleMatrix[Index]<< "  ║";
+        }
+        cout << endl << CenterDivision;
+    }
+    cout << endl << LowerDivision;
+}*/
